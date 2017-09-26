fcc3dbd073f19db52918007aefb584fd821c0131
Garrett Bigart <gbigart@gmail.com>
2017-09-26 15:49:28 -0700

    Add My First Challenge Completed

---
+++ MyFirstChallenge/MyFirstChallenge/Default.aspx.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace MyFirstChallenge
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void fortuneButton_Click(object sender, EventArgs e)
+        {
+            string age = ageTextBox.Text;
+            string cash = cashTextBox.Text;
+
+            string fortune = "At " + age + " years old, I would have expected you to have more than $" + cash + " in your pocket.";
+
+            fortuneLabel.Text = fortune;
+        }
+    }
+}
