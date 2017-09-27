c365afd0d4cc1c436f9590056f3698050ae5efeb
Garrett Bigart <gbigart@gmail.com>
2017-09-27 15:30:00 -0700

    Add project 008 and 009

---
+++ Course/CS-ASP_008/CS-ASP_008/Default.aspx.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace CS_ASP_008
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            /*
+            int i;
+            i = "hello world";
+            
+
+             upcasting (implicit)
+            int i = 2000000000;
+            long j = i;
+            
+
+             downcasting (explicit)
+            long i = 2000000000;
+            int j = (int)i;
+            
+
+            double k = 2.5;
+            int j = (int)k;
+
+            //resultLabel.Text = j;
+            //resultLabel.Text = (string)j;    [doesn't work]
+            resultLabel.Text = j.ToString();
+            */
+
+            string i = inputTextBox.Text;
+            int j = int.Parse(i);
+            int k = j + 1;
+            resultLabel.Text = k.ToString();
+        }
+    }
+}
---
+++ Course/CS-ASP_009/CS-ASP_009/Default.aspx.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace CS_ASP_009
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            int i = 1;
+            int j = 2;
+            //int result = i + j;
+            //int result = i - j;
+            //int result = i * j;
+            //int result = i / j;
+
+            /*
+            i = i + 1;
+            i += 5;
+            i++;
+            i--;
+            */
+
+            //int myInteger = (5 + 1) * 7;
+            //resultLabel.Text = myInteger.ToString();
+
+            //double myDouble = 5.5;
+            int myInteger = 7;
+            int myOtherInteger = 4;
+
+            //double myResult = myDouble + myInteger;
+            //int myResult = (int)myDouble + myInteger;
+            //resultLabel.Text = myResult.ToString();
+            //int myResult = myInteger / myOtherInteger;    This still truncates 
+            //double myResult = (double)myInteger / (double)myOtherInteger;
+
+            //resultLabel.Text = myResult.ToString();
+
+
+            /*  Creates an overflow
+            int firstNumber = 200000000;
+            int secondNumber = 2000000000;
+            int resultNumber = firstNumber + secondNumber;
+        
+                Fixes the overflow
+            long firstNumber = 200000000;
+            long secondNumber = 2000000000;
+            long resultNumber = firstNumber + secondNumber;
+
+            int firstNumber = 200000000;
+            int secondNumber = 2000000000;
+            int resultNumber = firstNumber + secondNumber;
+            */
+
+            int firstNumber = 200000000;
+            int secondNumber = 2000000000;
+
+            long resultNumber;
+
+            checked
+            {
+                resultNumber = firstNumber + secondNumber;
+            }
+
+            resultLabel.Text = resultNumber.ToString();
+            
+
+
+        }
+    }
+}
