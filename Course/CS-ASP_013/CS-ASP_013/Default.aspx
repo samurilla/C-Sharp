1ff11b2e7733035c8da02fd06ef907cd6bc6d998
Garrett Bigart <gbigart@gmail.com>
2017-09-28 19:40:28 -0700

    cs-asp-013 complete

---
+++ Course/CS-ASP_013/CS-ASP_013/Default.aspx
@@ -0,0 +1,28 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="CS_ASP_013.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            Is
+            <asp:TextBox ID="firstTextBox" runat="server"></asp:TextBox>
+&nbsp;<asp:Label ID="comparisonTypeLabel" runat="server" Text=""></asp:Label>
+&nbsp;<asp:TextBox ID="secondTextBox" runat="server"></asp:TextBox>
+            ?<br />
+            <br />
+            <asp:CheckBox ID="checkBox" runat="server" Text="I am checked" />
+            <br />
+            <br />
+            <asp:Button ID="okButton" runat="server" Text="OK" OnClick="okButton_Click" />
+            <br />
+            <br />
+            <asp:Label ID="resultLabel" runat="server" Text=""></asp:Label>
+        </div>
+    </form>
+</body>
+</html>
---
+++ Course/CS-ASP_013/CS-ASP_013/Default.aspx.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace CS_ASP_013
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            //comparisonTypeLabel.Text = "equal to";
+            //comparisonTypeLabel.Text = "not equal to";
+            comparisonTypeLabel.Text = "greater than";
+        }
+
+        protected void okButton_Click(object sender, EventArgs e)
+        {
+            resultLabel.Text = "";
+
+            //resultLabel.Text = (firstTextBox.Text == secondTextBox.Text) ? "Yes" : "No";
+
+            //resultLabel.Text = (firstTextBox.Text != secondTextBox.Text) ? "Yes" : "No";
+
+            // resultLabel.Text = (firstTextBox.Text > secondTextBox.Text) ? "Yes" : "No";
+
+            //int first = int.Parse(firstTextBox.Text);
+            //int second = int.Parse(secondTextBox.Text);
+            //resultLabel.Text = (first > second) ? "Yes" : "No";
+            //resultLabel.Text = (first < second) ? "Yes" : "No";
+            //resultLabel.Text = (first >= second) ? "Yes" : "No";
+            //resultLabel.Text = (first <= second) ? "Yes" : "No";
+
+
+            //resultLabel.Text = (!checkBox.Checked) ? "Yes" : "No";
+
+            /*
+            if (checkBox.Checked
+                && firstTextBox.Text == "Bob"
+                && secondTextBox.Text == "Tabor")
+            {
+                resultLabel.Text = "Perfect trifecta!";
+
+            }
+            */
+
+            /*
+            if (checkBox.Checked
+                || firstTextBox.Text == "Bob"
+                || secondTextBox.Text == "Tabor")
+            {
+                resultLabel.Text = "One out of three aint bad.";
+            }
+            */
+
+            if (checkBox.Checked
+                || firstTextBox.Text == "Bob"
+                && secondTextBox.Text == "Tabor")
+            {
+                resultLabel.Text = "Two out of three aint bad.";
+            }
+        }
+    }
+}
