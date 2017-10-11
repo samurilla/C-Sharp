863b4217e732070512b64bf17984dc7d7e998a2e
Garrett Bigart <gbigart@gmail.com>
2017-10-11 16:30:44 -0700

    Add ChallengeDaysBetweenDates

---
+++ ChallengeDaysBetweenDates/ChallengeDaysBetweenDates/Default.aspx.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ChallengeDaysBetweenDates
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void okButton_Click(object sender, EventArgs e)
+        {
+
+
+            DateTime firstDate = firstCalendar.SelectedDate;
+            DateTime secondDate = secondCalendar.SelectedDate;
+            TimeSpan calendarTimeSpan;
+
+            if (firstDate >= secondDate) {
+                calendarTimeSpan = firstDate - secondDate;
+                resultLabel.Text = calendarTimeSpan.Days.ToString();
+            }
+            else if (firstDate < secondDate)
+            {
+                calendarTimeSpan = secondDate - firstDate;
+                resultLabel.Text = calendarTimeSpan.Days.ToString();
+            }
+            
+        }
+    }
+}
--- Course/CS-ASP-015/CS-ASP-015/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CS_ASP_015.Global" Language="C#" %>
